99a37a6dca35cd3c6ae91248ec6e729ce010b759
RangaSai43 <34965092+RangaSai43@users.noreply.github.com>
2019-08-10 15:12:56 +0530

    Update Freecoffee.cpp

--- Freecoffee.cpp
+++ Freecoffee.cpp
@@ -2,21 +2,24 @@
 using namespace std;
 
 int main() {
-int x;
-float p;
-float discount;
-//Taking Input values of price and discount rates
-cin>>x;
-cin>>p;
-int sum=x;
-//Total value of percent
-p=(p/100);
-while(x>0)
-{
-    discount=x*p;
-    x=int(x-discount);
-    sum+=x;
-}
-cout<<sum<<endl;
+    int x;
+    float p,discount;
+    //Taking Input values of price and discount rates
+    cin>>x>>p;
+    int sum=x;
+    //Total value of percent
+    p=(p/100);
+    //Process of calculating Total cost it took
+    while(x>0)
+    {
+        //Discount=Price x Discount rate
+        discount=x*p;
+        //The new price of the coffee for you
+        x=int(x-discount);
+        //The total cost until now
+        sum+=x;
+    }
+    //Print the final sum
+    cout<<sum<<endl;
 	return 0;
 }
